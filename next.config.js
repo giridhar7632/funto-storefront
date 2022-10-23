df655e0dd588a6ea0a50e50e180f0c6cc303259d
Talla Giridhar <giridhar.talla2002@gmail.com>
2022-10-23 20:23:03 +0530

    update config

--- next.config.js
+++ next.config.js
@@ -5,8 +5,10 @@ module.exports = withStoreConfig({
   features: store.features,
   reactStrictMode: true,
   images: {
-    domains: ["medusa-public-images.s3.eu-west-1.amazonaws.com", "localhost"],
+    domains: [
+      "medusa-public-images.s3.eu-west-1.amazonaws.com",
+      "localhost",
+      "api.multiavatar.com",
+    ],
   },
 })
-
-console.log("next.config.js", JSON.stringify(module.exports, null, 2))
